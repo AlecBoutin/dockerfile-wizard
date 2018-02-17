543b5c39c57fe786841b6f5e2ef2ca65634025f8
Alec Boutin <alec.boutin@motionloft.com>
2018-02-17 18:24:13 -0500

    --

--- install-nunit.sh
+++ install-nunit.sh
@@ -3,5 +3,5 @@
 set -e
 
 wget https://github.com/nunit/nunit-console/releases/download/3.8/NUnit.Console-3.8.0.zip
-unzip NUnit.Console-3.8.0.zip
-ln -s `pwd`/NUnit/nunit3-console.exe /bin/nunit3-console
+unzip NUnit.Console-3.8.0.zip -d /NUnit/
+ln -s /NUnit/nunit3-console.exe /bin/nunit3-console
