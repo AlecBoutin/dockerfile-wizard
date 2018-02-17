fef69822c0c434337f8411679da66f8fe3058b24
Alec Boutin <alec.boutin@motionloft.com>
2018-02-17 15:20:57 -0500

    --

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM circleci/node:8.9-stretch
 
+RUN whoami
 COPY install-mono.sh install-mono.sh
 RUN /bin/bash install-mono.sh
--- install-mono.sh
+++ install-mono.sh
@@ -3,13 +3,13 @@
 set -e
 
 apt-get update
-apt-get install curl libunwind8 gettext apt-transport-https
+apt-get install -y curl libunwind8 gettext apt-transport-https
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian stable-stretch main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb http://download.mono-project.com/repo/debian stable-stretch main" | tee /etc/apt/sources.list.d/mono-official-stable.list
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/dotnetdev.list
 apt-get update
 
-apt-get install mono-complete nuget
+apt-get install -y mono-complete nuget
