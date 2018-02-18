48699d3c43be9640039e1b1c461dc8223cc292b1
Alec Boutin <alec.boutin@gmail.com>
2018-02-17 23:22:06 -0500

    actually include all the unity deps

--- install-unity.sh
+++ install-unity.sh
@@ -2,7 +2,42 @@
 
 set -e
 
-apt-get install -y libgtk2.0-0 libsoup2.4-1 libarchive13 libpng16-16
+apt-get install -y libgtk2.0-0 libsoup2.4-1 libarchive13 libpng16-16 \
+    gconf-service \
+    lib32gcc1 \
+    lib32stdc++6 \
+    libasound2 \
+    libc6 \
+    libc6-i386 \
+    libcairo2 \
+    libcap2 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libfontconfig1 \
+    libfreetype6 \
+    libgcc1 \
+    libgconf-2-4 \
+    libgdk-pixbuf2.0-0 \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libglu1-mesa \
+    libnspr4 \
+    libnss3 \
+    libpango1.0-0 \
+    libstdc++6 \
+    libx11-6 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxtst6 \
+    zlib1g \
+    debconf
 
 chmod +x UnitySetup-2017.3.1f1
 echo "y" | ./UnitySetup-2017.3.1f1 --unattended --install-location=/Unity
